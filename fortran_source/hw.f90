7b77bf342c44f72b7b05c7156f61a7530bea1a65
Matthias Pickem <matthias.pickem@gmail.com>
2018-07-14 17:36:48 +0200

    first working numpy imitation in fortran

--- fortran_source/hw.f90
+++ fortran_source/hw.f90
@@ -1,7 +1,7 @@
 real(8) function hw1(r1,r2)
   implicit none
   real(8) :: r1,r2
-  hw1=sin(r1+r2)
+  hw1=(r1+r2)
   return
 end function hw1
 
@@ -21,3 +21,15 @@ subroutine hw3(a,b,c,d)
   d = a-b
   return
 end subroutine hw3
+
+subroutine array_addition(xs,ys,a,b,c)
+  implicit none
+  integer, intent(in) :: xs, ys ! sizes
+  complex(8), dimension(xs,ys), intent(in) :: a,b
+  complex(8), dimension(xs,ys), intent(inout) :: c
+  integer i,j
+
+  c=a+b
+  return
+
+end subroutine array_addition
--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 
 from __future__ import print_function, division, absolute_import
 import numpy as np
-from fortran_source import hw
+from fortran_source.hw import *
 
-print(hw.hw1(0,0.1))
-print(hw.hw2(0,0.1))
-print(hw.hw3(1+1j, 1+0.5*1j))
+# from fortran_source.hw import * -> reference as hw1
+# from fortran_source import hw -> reference as hw.hw1
+# import fortran_source.hw -> reference as fortran_source.hw.hw1
+
+print(hw1(0,0.1))
+print(hw2(0,0.1))
+print(hw3(1+1j, 1+0.5*1j))
+
+print(array_addition.__doc__)
+
+a = np.ones((3,9), dtype=np.complex128, order='F')
+b = np.full((3,9), fill_value=1.43+0.5j, dtype=np.complex128, order='F')
+c = np.empty_like(a, dtype=np.complex128, order='F')
+
+# array_addition([[1,2,3],[1,2,3]],[[2,3,4],[2,3,4]],[[0,0,0],[0,0,0]])
+array_addition(a,b,c)
+print(c)
