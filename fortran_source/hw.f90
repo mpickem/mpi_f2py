58d6886df550a3b45a3b996200825e6a3b66ee0e
Matthias Pickem <matthias.pickem@gmail.com>
2018-07-14 21:55:25 +0200

    Added prototypical CLI; running lapack interfaces
    Lapack interfaces are not really necessary
    since we have the scipy interfaces anyways ...

--- fortran_source/hw.f90
+++ fortran_source/hw.f90
@@ -22,14 +22,52 @@ subroutine hw3(a,b,c,d)
   return
 end subroutine hw3
 
-subroutine array_addition(xs,ys,a,b,c)
+subroutine array_addition(a,b,c,xs,ys) ! put them at the end because they will be optioinal anyways
   implicit none
   integer, intent(in) :: xs, ys ! sizes
   complex(8), dimension(xs,ys), intent(in) :: a,b
   complex(8), dimension(xs,ys), intent(inout) :: c
-  integer i,j
-
   c=a+b
   return
-
 end subroutine array_addition
+
+subroutine dlapack_mul(a,b,c,rows_a,columns_a,columns_b)
+  implicit none
+  integer, intent(in)                                     :: rows_a,columns_a,columns_b
+  real(8), dimension(rows_a,columns_a), intent(in)     :: a
+  real(8), dimension(columns_a, columns_b), intent(in) :: b
+  real(8), dimension(rows_a, columns_b), intent(inout) :: c
+  call dgemm('n', 'n', rows_a, columns_b, columns_a, 1.d0, a, rows_a, b, columns_a, 0.d0, c, rows_a)
+  return
+end subroutine
+
+subroutine zlapack_mul(a,b,c,rows_a,columns_a,columns_b)
+  implicit none
+  integer, intent(in)                                     :: rows_a,columns_a,columns_b
+  complex(kind=8), dimension(rows_a,columns_a), intent(in)     :: a
+  complex(kind=8), dimension(columns_a, columns_b), intent(in) :: b
+  complex(kind=8), dimension(rows_a, columns_b), intent(inout) :: c
+  call zgemm('n', 'n', rows_a, columns_b, columns_a, 1.d0, a, rows_a, b, columns_a, 0.d0, c, rows_a)
+  return
+end subroutine
+
+subroutine inverse_matrix_z(M, ierr, ndim)
+  implicit none
+  integer, intent(in)                                   :: ndim
+  integer, intent(out)                                  :: ierr
+  complex(kind=8), dimension(ndim,ndim), intent (inout) :: M
+  integer                                               :: lwork
+  integer,        allocatable                           :: ipiv(:)
+  complex(kind=8), allocatable                          :: work(:)
+  complex(kind=8)                                       :: work_query(1)
+  allocate(ipiv(ndim))
+  call zgetrf(ndim,ndim,M,ndim,ipiv,ierr)
+  if (ierr .ne. 0) return
+  call zgetri(ndim,M,ndim,ipiv,work_query,-1,ierr) ! query for optimal work space
+  if (ierr .ne. 0) return
+  lwork = int(work_query(1))
+  allocate(work(lwork))
+  call zgetri(ndim,M,ndim,ipiv,work,lwork,ierr)
+  if (ierr .ne. 0) return
+  deallocate(ipiv,work)
+end subroutine
--- main.py
+++ main.py
@@ -5,34 +5,62 @@ import numpy as np
 import mpi4py # for information
 from mpi4py import MPI
 from fortran_source.hw import *
+import sys
+import time
 
 # from fortran_source.hw import * -> reference as hw1
 # from fortran_source import hw -> reference as hw.hw1
 # import fortran_source.hw -> reference as fortran_source.hw.hw1
 
-print(mpi4py.get_include())
-print(mpi4py.get_config())
 
+################################################################################
+## for future MPI stuff
+################################################################################
+# print(mpi4py.get_include())
+# print(mpi4py.get_config())
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 print('rank: ', rank, 'size: ', size)
-print()
-
-
+################################################################################
 
 
 print(hw1(0,0.1))
 print(hw2(0,0.1))
 print(hw3(1+1j, 1+0.5*1j))
 
-print(array_addition.__doc__)
-
-# a = np.ones((3,9), dtype=np.complex128, order='F')
+# print(array_addition.__doc__)
 a = np.arange(27, dtype=np.complex128).reshape((3,9), order='F')
 b = np.full((3,9), fill_value=1.43+0.5j, dtype=np.complex128, order='F')
 c = np.empty_like(a, dtype=np.complex128, order='F')
 
+
 # array_addition([[1,2,3],[1,2,3]],[[2,3,4],[2,3,4]],[[0,0,0],[0,0,0]])
 array_addition(a,b,c)
-print(c)
+
+print()
+print()
+
+print(dlapack_mul.__doc__)
+matrix_left = np.ones((10,1000), dtype=np.float64, order='F')
+matrix_right = np.ones((1000,10), dtype=np.float64, order='F')
+c = np.empty((10,10), dtype=np.float64, order='F')
+dlapack_mul(matrix_left, matrix_right, c)
+
+matrix_left = np.ones((10,1000), dtype=np.complex128, order='F')
+matrix_right = np.ones((1000,10), dtype=np.complex128, order='F')
+c = np.zeros((10,10), dtype=np.complex128, order='F')
+zlapack_mul(matrix_left, matrix_right, c)
+
+
+a = np.eye(5, dtype=np.complex128, order='F')*2
+a[0,0] = 0
+
+print('inverting....')
+if (inverse_matrix_z(a)):
+  print('Singular matrix')
+  sys.exit()
+else:
+  print('Good to go')
+
+print(a)
