a89568cf7b1ae9925c6b3c4b36dd05752b4337a3
Matthias Pickem <matthias.pickem@gmail.com>
2018-07-15 12:57:08 +0200

    Running Gatherv, Bcast.

--- main.py
+++ main.py
@@ -38,6 +38,9 @@ for i in xrange(mpi_size-1):
   displ.append(rct[i]+displ[i])
 rct.append(data_interval - displ[mpi_size-1])
 
+rct = np.array(rct)
+displ = np.array(displ)
+
 qstart = displ[mpi_rank]
 qstop  = displ[mpi_rank] + rct[mpi_rank]
 
@@ -61,10 +64,14 @@ for i in xrange(mpi_size):
 comm.barrier()
 
 
-data_vec = np.zeros((30,30,qstop-qstart))
-data_everyone = np.ones((10,10,data_interval), dtype=np.int64)
 
 
+########################################
+# TESTING REDUCE
+########################################
+
+data_everyone = np.ones((10,10,data_interval), dtype=np.int64, order='F')
+# Reducing stuff
 if (mpi_rank == master):
   comm.Reduce(MPI.IN_PLACE, [data_everyone, MPI.INT64_T], op=MPI.SUM, root=master)
 else:
@@ -73,6 +80,68 @@ else:
 if (mpi_rank == master):
   print(data_everyone[...,0])
   sys.stdout.flush()
+
+comm.barrier()
+########################################
+
+
+
+
+
+########################################
+# TESTING GATHERV
+########################################
+
+data_vec = np.ones((2,2,qstop-qstart), dtype=np.float64, order='F') * (mpi_rank+1)
+
+print(data_vec)
+sys.stdout.flush()
+comm.barrier()
+print()
+
+# Gathering stuff
+if (mpi_rank == master): # so not every core has to allocate this memory
+  # if Fortran order we have to put the parllelized index on the right
+  data_gather = np.zeros((2,2,data_interval), dtype=np.float64, order='F')
+else:
+  # so the namespace is there for the Gatherv command
+  data_gather = np.zeros((1,1,1), dtype=np.float64, order='F')
+
+# this for is because of the 2,2 in front
+comm.Gatherv([data_vec, rct[mpi_rank]*4, MPI.DOUBLE] , [data_gather, rct*4, displ*4, MPI.DOUBLE], root=master)
+
+if (mpi_rank == master):
+  print(data_gather)
+  sys.stdout.flush()
+
+comm.barrier()
+########################################
+
+
+
+########################################
+# TESTING BCAST
+########################################
+
+
+data = np.zeros((3,3,data_interval), dtype=np.complex128, order='F')
+if (mpi_rank == master):
+  data = np.full_like(data, fill_value=2.3+1j, dtype=np.complex128, order='F')
+
+if (mpi_rank == 1):
+  print('data before bcast', data)
+  sys.stdout.flush()
+comm.barrier()
+comm.Bcast([data, MPI.COMPLEX16], root=master) # capital B -> in place (i.e. same name
+# small b -> returns mpi4py data object ... no
+if (mpi_rank == 1):
+  print('data after bcast', data)
+  sys.stdout.flush()
+  print(data.flags)
+comm.barrier()
+
+########################################
+
 
 sys.exit()
 
